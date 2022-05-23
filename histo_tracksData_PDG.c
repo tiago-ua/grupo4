093cea737bb623c2a75a00ed5aaca9cfc87e1132
Tiago Miguel Silva Rodrigues <tmsr@Tiagos-MacBook-Air.local>
2022-05-23 20:38:30 +0100

    Histogramas com e sem filtragem com base em particle PDG.

---
+++ histo_tracksData_PDG.c
@@ -0,0 +1,36 @@
+void histo_tracksData_PDG(){
+    TFile *ficheiro=new TFile("AmberTarget_Run_0.root","READ");
+    TFile *ficheiroGravar=new TFile("Analise.root","RECREATE");
+    TTree *dados=(TTree*)ficheiro->Get("tracksData");
+
+    Int_t nBins=600;
+    Double_t minBin=0.0;
+    Double_t maxBin=60000;
+
+    Int_t nHistos=4;
+    TH1D* histoDetetor[nHistos];
+    TCanvas *canvas[nHistos];
+    TString branchName;
+    TString canvasName;
+
+    //TArray color = ["kRed", "kBlue", "kGreen", "kBlack"];
+
+    THStack *hs = new THStack("hs","Stacked 1D histograms");
+        
+
+    for (Int_t i=0;i<nHistos; i++){
+        TString histoName="histoDetetor"+TString::Itoa(i,10);
+        histoDetetor[i]=new TH1D(histoName, histoName, nBins, minBin, maxBin);
+        branchName="EdepDet"+TString::Itoa(i,10)+"_keV";
+        //canvasName="canvas"+TString::Itoa(i,10);
+
+        histoDetetor[i]->SetFillColor(i);
+        dados->Draw(branchName+">>"+histoName,branchName+">0 && (particlePDG == 13 || particlePDG == -13)","goff");
+        histoDetetor[i]->SetTitle(histoName);
+        histoDetetor[i]->Write();        
+        hs->Add(histoDetetor[i]);
+        
+    }
+    hs->Draw("Nostack");	
+    hs->SetTitle("Tracks Data PDG 13 e -13");
+}
