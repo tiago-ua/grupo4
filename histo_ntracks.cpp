ed88547e0f2f340dbc0830d9d8a316c00cf1725f
Tiago Miguel Silva Rodrigues <tmsr@Tiagos-Air.lan>
2022-05-23 23:40:48 +0100

    Adicionei .gitignore e README

---
+++ histo_ntracks.cpp
@@ -0,0 +1,39 @@
+void histo_ntracks(){
+
+TFile *ficheiro=new TFile("AmberTarget_Run_0.root","READ");
+TFile *ficheiroGravar=new TFile("Analise.root","RECREATE");
+TTree *dados=(TTree*)ficheiro->Get("nTracks");
+
+Int_t nBins=300;
+Double_t minBin=0.0;
+Double_t maxBin=600;
+
+Int_t nHistos=4;
+TH1D* histoDetetor[nHistos];
+TCanvas *canvas[nHistos];
+TString branchName;
+TString canvasName;
+
+//TArray color = ["kRed", "kBlue", "kGreen", "kBlack"];
+
+THStack *hs = new THStack("hs","Stacked 1D histograms");
+	
+
+for (Int_t i=0;i<nHistos; i++){
+	TString histoName="histoDetetor"+TString::Itoa(i,10);
+	histoDetetor[i]=new TH1D(histoName, histoName, nBins, minBin, maxBin);
+	branchName="EdepDet"+TString::Itoa(i,10)+"_keV";
+	//canvasName="canvas"+TString::Itoa(i,10);
+	histoDetetor[i]->SetFillColor(i);
+	dados->Draw(branchName+">>"+histoName,branchName+">0","goff");
+	histoDetetor[i]->SetTitle(histoName);
+	histoDetetor[i]->Write();
+	hs->Add(histoDetetor[i]);
+	
+}
+hs->Draw("Nostack");	
+hs->SetTitle("nTracks data");
+}
+
+
+
