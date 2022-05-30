39ab7f657d5c65e1ea42ef4d71384b1b157c91ea
nunovc <102529251+nunovc@users.noreply.github.com>
2022-05-30 14:21:48 +0100

    Add files via upload

---
+++ Detector_0.C
@@ -0,0 +1,31 @@
+void 30_maio(){
+
+TFile * ficheiro= new TFile("AmberTarget_Run_0.root","READ");
+TFile * ficheiroGravar= new TFile("Analise.root","RECREATE");
+TTree * dados= (TTree*)  ficheiro -> Get("edep_Per_Event");
+Double_t detector0;
+Double_t detector1;
+Double_t detector2;
+Double_t detector3;
+Double_t Soma;
+
+dados->SetBranchAddress("detector0",&detector0);
+dados->SetBranchAddress("detector1",&detector1);
+dados->SetBranchAddress("detector2",&detector2);
+dados->SetBranchAddress("detector3",&detector3);
+
+
+Long64_t  N = dados-> GetEntries();
+TTree* newTree = new TTree("newTree","newTree");			//Completar
+newTree -> Branch("Soma", &Soma, "Soma /D");
+
+for (Int_t i=0; i< N; i++){
+	dados->GetEntry(i);
+	Soma=detector0+detector1+detector2+detector3;
+	newTree -> Fill();
+	
+} 
+
+newTree -> Write();
+
+}
